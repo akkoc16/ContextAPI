71725e8ee0d04e5040aff5bdf84089fd4a304cee
akkoc16 <m.enver.akkoc@gmail.com>
2020-06-27 15:50:40 +0300

    slideradded

--- src/pages/VoteFilter.js
+++ src/pages/VoteFilter.js
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react'
-import { SafeAreaView, Text, View ,FlatList, Button} from 'react-native'
+import { SafeAreaView, Text, View ,FlatList, Button, Dimensions} from 'react-native'
+import Slider from '@react-native-community/slider';
 import Context from '../context/store'
 
 const VoteFilter = props => {
@@ -8,11 +9,17 @@ const VoteFilter = props => {
 
     return (
         <SafeAreaView>
-            <Text>VoteFilter</Text>
-            <Button title="Filtrele" onPress={()=> dispatch({type:"FILTER_CATS", vote:5})}></Button>
+            <Slider
+                style={{width: Dimensions.get("window").width*0.9,alignSelf:'center'}}
+                minimumValue={0}
+                maximumValue={6}
+                onValueChange={(value)=>dispatch({type:"FILTER_CATS", vote:value})}
+                step={1}
+            />
             <FlatList
                 keyExtractor={(item,index)=>index.toString()}
                 data={state.filteredCats}
+                ItemSeparatorComponent={() => <View style={{ borderWidth: 1, borderColor: "#e0e0e0", marginVertical: 5 }} />}
                 renderItem={({item})=> <Text style={{padding:10}}>{item.text}</Text>}></FlatList>
         </SafeAreaView>
     )
